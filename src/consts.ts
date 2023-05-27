0e3d38c42b33f2b9a03c52d56ac61e666695cc60
no2499 <ihaveno2499@gmail.com>
2023-05-27 18:35:21 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '顆顆 BLOG';
+export const SITE_DESCRIPTION = '顆顆的生活筆記';
