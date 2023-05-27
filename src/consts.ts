b1180750e78fd30d274332c991229bffd586cac4
no2499 <ihaveno2499@gmail.com>
2023-05-27 18:59:38 +0800

    a

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,3 @@
 // Place any global configuration here
-export const SITE_TITLE = 'blogAstro';
-export const SITE_DESCRIPTION = 'blogAstro Description';
+export const SITE_TITLE = '顆顆ㄉ生活記事';
+export const SITE_DESCRIPTION = 'ㄎㄎ keke.day';
